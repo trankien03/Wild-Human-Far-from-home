17863022ccaa4bb4a9d2ee8d436b39802dff9af1
Nahoo a.k.a Keng <meltryllis82@gmail.com>
2024-06-03 16:53:26 +0700

    fix smth

--- Assets/GameComponent/Scripts/AttackForPlayer.cs
+++ Assets/GameComponent/Scripts/AttackForPlayer.cs
@@ -2,31 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour
+public class AttackForPlayer : MonoBehaviour
 {
     public float attackDamage = 20.0f;
-    public Vector2 Knockback = new Vector2(2, 1);
+    public  Vector2 Knockback = new Vector2 (2,1);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Damageable damageable = collision.GetComponent<Damageable>();
 
-        if (damageable != null)
+        if (damageable != null) 
         {
-            Vector2 delivererdKnockback = transform.parent.localScale.x > 0 ? Knockback : new Vector2(-Knockback.x, Knockback.y);
-            bool gothit = damageable.Hit(attackDamage, delivererdKnockback);
+            Vector2 delivererdKnockback = transform.parent.rotation.y == 0f ? Knockback : new Vector2(-Knockback.x, Knockback.y);
+            bool gothit = damageable.Hit(attackDamage, delivererdKnockback);    
             if (gothit) Debug.Log(collision.name + "hiting" + attackDamage);
         }
     }
